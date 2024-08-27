d01075c7196d94d118f279f8ec464516f33a43c4
Harsha V <harsha.vishwanath2004@gmail.com>
2024-08-27 12:11:13 +0530

    fixed forward prop

--- forward_prop.py
+++ forward_prop.py
@@ -4,28 +4,30 @@ def sigmoid(x):
     return 1/(1+(np.exp(-x)))
 
 class ForwardProp:
-    def forward_prop(self, inp):
-        if len(inp)==1:
-            z1 = np.dot(self.input_weights, inp[0])
-        else:
-            z1 = np.dot(inp, self.input_weights)
-        a1 = sigmoid(z1)
+    def forward_prop(self, inp_vector):
+        self.intermediary_z=[]
+        total_outputs=[]
+        for inp in inp_vector:
+            if len(inp)==1:
+                z_inp=np.dot(self.input_weights, inp[0])
+            else:
+                z_inp=(np.dot(inp,self.input_weights))
 
-        current_a = a1
-       
-        self.intermediary_z = []
-        self.intermediary_z.append(z1)
+            current_a=sigmoid(z_inp)
+            int_z=[z_inp]
 
-        for i in range(len(self.hidden_weights)):
-            weights = self.hidden_weights[i]
-            bias=self.hidden_bias[i]
-            
-            current_z = np.dot(weights, current_a)+bias
-            self.intermediary_z.append(current_z)
+            for i in range(len(self.hidden_weights)):
+                current_z=np.sum(np.dot(current_a,self.hidden_weights[i]))+self.hidden_bias[i]   
 
-            current_a = sigmoid(current_z)
-        
-        output = sigmoid(np.dot(self.output_weights, current_a) + self.output_bias)
+                int_z.append(current_z)
+                current_a=sigmoid(current_z)  
 
-        self.intermediary_z=np.array(self.intermediary_z)        
-        return output
+            if len(self.output_weights[0])==1:
+                output_z=np.sum(np.dot(self.output_weights.flatten(),current_a))+self.output_bias
+            else:
+                output_z=np.sum(np.dot(self.output_weights.T,current_a))+self.output_bias
+            print(output_z)
+            total_outputs.append(sigmoid(output_z))
+            self.intermediary_z.append(int_z)
+        self.intermediary_z=np.array(self.intermediary_z)
+        return total_outputs
--- np_testing.py
+++ np_testing.py
@@ -1,9 +1,13 @@
 import numpy as np
 from nodes import NeuralNetwork
 
-x=np.random.randn(10,784)
-y=np.random.randn(784)
+x=np.random.randn(200,784,)
+y=np.random.randn(200)
 
 forward=NeuralNetwork()
-forward.init_params(10,10,784,10)
+forward.init_params(10,6,784,2)
 forward.forward_prop(x)
+
+a=np.array([1,2,3])
+
+print([a])
